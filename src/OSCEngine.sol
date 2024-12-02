df41d677ef9d2667bfce9916667375c62cbfcdc7
owanemi <owanemi1741@gmail.com>
2024-12-02 17:15:22 +0100

    added user collateral balance getter function

--- src/OSCEngine.sol
+++ src/OSCEngine.sol
@@ -311,6 +311,9 @@ contract OSCEngine is ReentrancyGuard {
         return address(i_osc);
     }
 
+    function getCollateralBalanceOfUser(address user, address token) external view returns (uint256) {
+        return s_collateralDeposited[user][token];
+    }
     function getCollateralTokenPriceFeed(address token) external view returns (address) {
         return s_priceFeeds[token];
     }
